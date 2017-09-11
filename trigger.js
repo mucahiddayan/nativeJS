684c03707168f3ee6c301ee4cf3c997ed671482c
mucahid <mucahid@dayan.one>
2017-09-11 12:25:38 +0200

    compability

--- trigger.js
+++ trigger.js
@@ -4,8 +4,9 @@
 class Trigger{
     fire(element,event){
         if ("createEvent" in document){
-            var evt = document.createEvent("HTMLEvents");
-            evt.initEvent(event, false, true);
+            /* var evt = document.createEvent("HTMLEvents");
+            evt.initEvent(event, false, true); */
+            var evt = new Event(event);
             element.dispatchEvent(evt);
         }else{
             element.fireEvent(`on${event}`); 
