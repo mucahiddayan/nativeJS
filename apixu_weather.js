ad7b646b561a5351cc21cb82046dbf149be7c8e7
mucahid <mucahid@dayan.one>
2017-10-10 17:49:49 +0200

    apixu plugin

--- apixu_weather.js
+++ apixu_weather.js
@@ -7,59 +7,47 @@ let Apixu = function(API_KEY){
     __apiURL = 'https://api.apixu.com/v1/forecast.json?',
     __apiKey = API_KEY,
     /* 'c6df1e406f204a32926120907170910' */
-
-    __defaultDisplayOptions = {
-        feelslike_c: '#feelslike_c',
-        feelslike_f: '#feelslike_f',
-        humidity: '#humidity',        
-        precip_in: '#precip_in',
-        precip_mm: '#precip_mm',
-        pressure_in: '#pressure_in',
-        pressure_mb: '#pressure_mb',
-        temp_c: '#temp_c',
-        temp_f: '#temp_f',
-        vis_km: '#vis_km',
-        vis_miles: '#vis_miles',
-        wind_degree: '#wind_degree',
-        wind_dir: '#wind_dir',
-        wind_kph: '#wind_kph',
-        wind_mph: '#wind_mph',
+    
+    __icons = {
+        precipitation: 'raindrop',
+        pressure: 'barometer',
+        wind:'small-craft-advisory',        
     },
-
+    
     __URL,
     __isLocationSet = false,
     __intervalTime = 0,
     __interval,
-
+    
     end; // variables end
-
+    
     // <--------------------------------------- PUBLIC FUNCTIONS
-
+    
     /**
-     * set the Location
-     * @param {string} location - Default: Berlin
-     */
+    * set the Location
+    * @param {string} location - Default: Berlin
+    */
     this.setLocation = function(location = 'Berlin'){
         __URL = `${__apiURL}key=${__apiKey}&q=${location}`;
         __isLocationSet = true;
         return this;
     }
-
+    
     /**
-     * @param {number} time - in minutes 
-     */
+    * @param {number} time - in minutes 
+    */
     this.updateIn = function(time){
         __intervalTime = time * 1000 * 60;
         return this;
     }
-
-    this.display = function(options = __defaultDisplayOptions){
+    
+    this.display = function(options = __icons){
         var init = function(){
             sendQuery().then(res=>{
                 if(res.error){
                     console.warn(res.error.message);return;
                 }
-                __display(res,options);
+                __display(res);
             }).catch(er=>console.warn(er));
         }
         try{
@@ -73,9 +61,11 @@ let Apixu = function(API_KEY){
         }catch(e){
             console.warn(e.name,e.message);
         }
-
+        
     }
-
+    
+    
+    
     // PUBLIC FUNCTIONS END ----------------------------------->
     //##########################################################
     // <-------------------------------------- PRIVATE FUNCTIONS
@@ -83,36 +73,100 @@ let Apixu = function(API_KEY){
         this.message = msg;
         this.name = 'LocationUnsetError';
     }
-
+    
     var OverageError = function(msg=''){
         this.message = msg;
         this.name = 'OverageError';
     }
+    
+    var __display = function(values){
+        console.log(values);
+        var displayElement = document.createElement('div');
+        displayElement.id = 'forecast-display';
+        
+        var    
+        value = values.current,    
+        img = `<img src="https:${values.current.condition.icon}"/>`,
+        box = `<div id="forecast-wrapper">
+        <div id="forecast">
+        <div id="icon">${img}</div>
+        <div id="temp" data-f="${value.temp_f}" data-c="${value.temp_c}" title="Temperature ${__temp}">${value.temp_c}</div>
+        <div id="feelslike" data-f="${value.feelslike_f}" data-c="${value.feelslike_c}" title="Feelslike">${value.feelslike_c}</div>
+        <div id="precipitation-wrapper">
+        <div id="precipitation-icon" title="Precipitation">${getIcon('raindrop')}</div>
+        <div id="precipitation" data-in="${value.precip_in}" data-mm="${value.precip_mm}" title="${__precip}">${value.precip_mm}</div>
+        </div>
+        <div id="pressure-wrapper">
+        <div id="pressure-icon" title="Pressure">${getIcon('barometer')}</div>
+        <div id="pressure" data-in="${value.pressure_in}" data-mb="${value.pressure_mb}" title="Pressure ${__pressure}">${value.pressure_mb}</div>
+        </div>
+        <div id="vis" data-km="${value.vis_km}" data-miles="${value.vis_miles}">${value.vis_km}</div>
+        <div id="wind-wrapper">
+        <div id="wind-icon" title="Wind/${__wind}">${getIcon('wind from-'+value.wind_degree+'-deg',value.wind_dir)}</div>
+        <div id="wind" data-kph="${value.wind_kph}" data-mph="${value.wind_mph}">${value.wind_kph}</div>
+        </div>
+        </div>
+        </div>`; // forecast wrapper ends
 
-    var __display = function(values,options){
-        console.log(values,options);
-        var text = `<img src="https:${values.current.condition.icon}"/>`;
-        for(let o in options){
-            if(options.length > values.length){
-                throw new OverageError('There is more display object than values to display');
-            }
-            var value = values.current[o];
-            text += `<div class="${o}-item item" title="${o}">
-                        <div class="${o}-item-icon">${getIcon(o)}</div>
-                        <div class="${o}-item-text">${value}</div>
-                        </div>`;
 
-            var displayElement = document.querySelector(options[o]);
-            if(displayElement){
-                displayElement.innerHTML = text;
-            }
+        
+        if(displayElement){
+            displayElement.innerHTML = box;
+        }
+        
+        if(document.getElementById('forecast-display') == null){
+            document.body.appendChild(displayElement);
+        }else{
+            document.getElementById('forecast-display').innerHTML = box;
+        }
+
+        decorate();
+        
+    }
+    
+
+    var __precip = 'in',
+    __pressure = 'in',
+    __wind = 'kph',
+    __vis = 'km',
+    __temp = 'C';
+    
+    var decorate = function(){
+        var toggleFeelslike = function(el){
+            console.log(this.dataset);
+        }
+        document.getElementById('feelslike').addEventListener('click',toggleFeelslike);
+
+        var togglePrecip = function(el){
+            console.log(this.dataset);
+        }
+        document.getElementById('precipitation').addEventListener('click',togglePrecip);
+
+        var togglePressure = function(el){
+            console.log(this.dataset);
+        }
+        document.getElementById('pressure').addEventListener('click',togglePrecip);
+
+        var toggleTemp = function(el){
+            console.log(this.dataset);
+        }
+        document.getElementById('temp').addEventListener('click',toggleTemp);
+
+        var toggleVis = function(el){
+            console.log(this.dataset);
+        }
+        document.getElementById('vis').addEventListener('click',toggleVis);
+
+        document.getElementById('wind').addEventListener('click',toggleWind);
+        var toggleWind = function(el){
+            console.log(el.dataset);
         }
     }
-
+    
     var getIcon = function(code,title=code){
         return `<i class="wi wi-${code}" title="${title}"></i>`;
     }
-
+    
     var sendQuery = function(){
         if(!__isLocationSet){
             throw new LocationUnsetError("Location has to be set to send request!");
@@ -125,6 +179,6 @@ let Apixu = function(API_KEY){
             }
         });
     }
-
+    
     // PRIVATE FUNCTIONS END ---------------------------------->
 }
--- weather.js
+++ weather.js
@@ -1,57 +1,230 @@
-class Weather{
+var Weather = function(){
+    /**
+    * __@var - private variable prefix
+    * 
+    */
+    var
+    __this = this,    
+    __apiUrl = `https://query.yahooapis.com/v1/public/yql?q=`,
     
-    constructor(){
-        this.results = [];
-        this.result;
-        this.apiUrl =  `https://query.yahooapis.com/v1/public/yql?q=`;
-        this.city;
-        this.countryCode;
-        this.locationSet = false;
-    }
+    __isLocationSet = false,
+    __city,
+    __countryCode,
+    __defaultDisplaySettings = {
+        atmosphere  : '#atmosphere',
+        astronomy   : '#astronomy',
+        temp        : '#temp',
+        forecast    : '#forecast',
+        wind        : '#wind',      
+    },
     
-    getUrl(){
-        let query   = `select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="${this.city},${this.countryCode}")`;            
+    __runs = {
+        atmosphere  : __atmosphere,
+        astronomy   : __astronomy,
+        temp        : __temp,
+        forecast    : __forecasts,
+        wind        : __wind,      
+    },
+    
+    __forecastLimit = 10,
+    __results = [],
+    __result;
+    
+    var getUrl = function(){
+        let query   = `select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="${__city},${__countryCode}")`;            
         query   = encodeURIComponent(query);
-        let url     = this.apiUrl + query + `&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`;
+        let url     = __apiUrl + query + `&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`;
         return url;
     }
-
-    sendQuery(){
-        if(!this.locationSet){console.warn('You have to set the location first');return;} // if location is not set yet, user will be warnt
-        fetch(this.getUrl()).then(res=>{
+    
+    var sendQuery = function(callback){
+        if(!__isLocationSet){console.warn('You have to set the location first');return false;} // if location is not set yet, user will be warnt
+        return fetch(getUrl()).then(res=>{
             try{
                 return res.json();
             }catch(e){
                 console.warn(e);
             }
-        }).then(res=>{
-            this.results.push(res);
-            this.result = res;
-            console.log(res);
-        }).catch(e=>console.warn(e));
+        });
+    }    
+    
+    var fahrenheitToCelcius = function(f,round=1){
+        return ((f-32) * 5/9).toFixed(round);
     }
     
-    setLocation(city='Berlin',countryCode='de'){
-        this.city = city;
-        this.countryCode = countryCode;
-        this.locationSet = true;
+    //****************PUBLIC FUNCTIONS****************//    
+    this.setLocation = function(city= 'Berlin',countryCode = 'de'){
+        __city = city;
+        __countryCode = countryCode;
+        __isLocationSet = true;
+        
         return this;     
     }
-
-    getWeather(){
-        if(!this.locationSet){console.warn('You have to set the location first');return;} // if location is not set yet, user will be warnt
-        this.sendQuery();
-        return `High: ${this.fahrenheitToCelcius(this.result.query.results.channel.item.forecast[0].high)}°C 
-        Low: ${this.fahrenheitToCelcius(this.result.query.results.channel.item.forecast[0].low)}°C
-                 ${this.result.query.results.channel.item.condition.text}`;
+    
+    this.setApiUrl = function(url){
+        __apiUrl = url;
     }
     
-    getResult(){
-        return this.result;
+    this.setForecastLimit = function(limit){
+        __forecastLimit = limit;
     }
     
-   
-    fahrenheitToCelcius(f){
-        return (f-32) * 5/9;
+    this.display = function(options = __defaultDisplaySettings){        
+        console.log(getUrl());
+        console.log(options);
+        if(sendQuery()){
+            sendQuery().then(res=>{
+                if(res.query.results == null){console.warn(`No results for "${__city}"`);return;}
+                var values = extractWeatherInformations(res);
+                console.log(values);
+                console.log(res);               
+                __display(options,values);
+            });
+        }
+    }
+    
+    this.getQuery = function(){
+        sendQuery().then(e=>console.log(e));
+    }
+    
+    var extractWeatherInformations = function(json){
+        try{
+            var temp = json.query.results.channel;
+        }catch(e){
+            console.warn(e);
+            return;
+        }
+        return {
+            astronomy   : temp.astronomy,
+            atmosphere  : temp.atmosphere,
+            temp        : {value:temp.item.condition.temp,code:temp.item.condition.code,text:temp.item.condition.text},
+            forecast    : temp.item.forecast,
+            wind        : temp.wind
+        };
+    }
+    
+    String.prototype.toCamelCase = function(){
+        return /\s/.test(this)?this.split(' ').reduce((f,s)=>f[0].toLowerCase()+f.substr(1,f.length-1)+s[0].toUpperCase()+s.substr(1,s.length-1)):this.toLowerCase();
+    }
+    
+    /**
+    * 
+    * @param {object<forecast>[]} forecast 
+    */
+    var __forecasts = function(forecasts){
+        var box = boxStart('forecast');
+        for(var forecast in forecasts){
+            if(forecast <= __forecastLimit){
+                box += __forecast(forecasts[forecast]);        
+            }
+        }
+        box += boxEnd();
+        return box;
+    }
+    
+    var __forecast = function(forecast){
+        var box = `<div class="forecast-item item">
+        <h2 title="${forecast.date}">${forecast.day}</h2>
+        <div id="forecast-icon">${getIcon(`yahoo-${forecast.code}`,forecast.text)}</div>
+        <div id="forecast-heigh" title="High">${fahrenheitToCelcius(forecast.high)}°C</div>
+        <div id="forecast-low" title="Low">${fahrenheitToCelcius(forecast.low)}°C</div>
+        </div>`;
+        return box;
+    }
+    
+    var __atmosphere = function(atmosphere){
+        return createItemFor('atmosphere',atmosphere);       
+    }
+    
+    var __astronomy = function(astronomy){
+        return createItemFor('astronomy',astronomy);
+    }
+    
+    var __temp = function(temp){
+        var box = boxStart('temp');        
+            box+= `<div class="temp-item item">
+                    <div class="temp-item-icon">${getIcon('yahoo-'+temp.code,temp.text)}</div>
+                    <div class="temp-item-chill" title="Temp">${fahrenheitToCelcius(temp.value)}°C</div>
+                    </div>`;
+        return box;
+    }
+    
+    var __wind = function(wind){
+        var box = boxStart('wind');
+        var icon = `wind from-${wind.direction}-deg`
+            box+= `<div class="wind-item item">
+                    <div class="wind-item-icon">${getIcon(icon,wind.direction)}</div>
+                    <div class="wind-item-chill" title="Speed">${wind.speed}km/h</div>
+                    <div class="wind-item-chill" title="Chill">${fahrenheitToCelcius(wind.chill)}°C</div>
+                    </div>`;
+        return box;
+    }
+    
+    var createItemFor = function(fr,parent){
+        var box = boxStart(fr);
+        for(var el in parent){ 
+            box += `<div class="${fr}-item item">
+            <div id="item-icon">${getIcon(el)}</div>
+            <div id="item-text">${parent[el]}</div>
+            </div>
+            `;
+        }
+        box += boxEnd();
+        return box;
+    }
+    
+    /**
+    * 
+    * @param {string} code 
+    * @param {string} title 
+    * wi wi-wind from-DIRECTION-deg
+    * wi wi-yahoo-CODE
+    * wi wi-NAME
+    */
+    var getIcon = function(code,title=code){
+        return `<div title="${title}" class="weather-icon"><i class="wi wi-${code}"></i></div>`;
+    }
+    
+    var boxStart = function(id){
+        return `<div id="${id}-wrapper"><div id="${id}">`;
+    }
+    
+    var boxEnd = function(){
+        return '</div></div>';
+    }    
+    
+    
+    /**
+    * 
+    * @param {Object} displays - name : CSS Selector - where the value has to be displayed on 
+    * @param {Object} values   - name : values - values to be displayed 
+    */
+    var __display = function(displays,values){
+        for(let o in displays){
+            if(displays.length >= __defaultDisplaySettings.length){console.warn('Flew over');return;}
+            var text = '';
+            switch(o){
+                case 'forecast':
+                text = __forecasts(values[o]);
+                break;
+                case 'atmosphere':
+                text = __atmosphere(values[o]);
+                break;
+                case 'astronomy':
+                text = __astronomy(values[o]);
+                break;
+                case 'temp':
+                text = __temp(values[o]);
+                break;
+                case 'wind':
+                text = __wind(values[o]);
+                break;
+            }
+            // text = __runs[o](values[o]);
+            if(document.querySelector(displays[o])){
+                document.querySelector(displays[o]).innerHTML = text;
+                // console.log(document.querySelector(displays[o]));
+            }                      
+        }
     }
 }
