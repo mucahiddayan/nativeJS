0ef304f29e50e6152544c268b67dde888e893cdd
mucahid <mucahid@dayan.one>
2017-11-23 15:47:28 +0100

    deep assign for object

--- Object.deepAssign.js
+++ Object.deepAssign.js
@@ -3,13 +3,7 @@
  * @version 1.0.0
  */
 
-const x = { a: { a: 1, c: 2 } }
-const y = { a: { b: 1, d: 4 }, b: { a: 2 } }
-const z = [2,3,4,6,7];
-const l = { a: { a: 2, d: 3 ,e: 5 },name:'mücahid',position:{left:10,right:{po:1}}}
-
-
-const deepAssign = (target = {}, ...sources) => {
+ export const deepAssign = (target = {}, ...sources) => {
     let src, copy,clone;
     for (let object of sources) {
         for (let p in object) {
